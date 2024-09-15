fc2016aeaa85856c92e75c61058cf4cf0b8720bd
Tosha <anton.primush123663@gmail.com>
2024-09-15 09:45:54 +0300

    Fixed error when minutes were month

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const moment = require("moment")
 
 function getDate(){
-    console.log(moment().format("YYYY/MM/DD HH:MM:SS"))
+    console.log(moment().format("YYYY/MM/DD hh:mm:ss"))
 }
 
 getDate()
